8395b6435908b49fcd992fa616af0febfeeaf127
PranavSurve-dev <pranavsurve444@gmail.com>
2026-09-23 12:24:46 +0530

    send data

--- 02_SendDataFromControllerToView/src/main/java/com/example/__controllertoview/TestController.java
+++ 02_SendDataFromControllerToView/src/main/java/com/example/__controllertoview/TestController.java
@@ -2,7 +2,9 @@ package com.example.__controllertoview;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -14,4 +16,24 @@ public class TestController {
 		model.addAttribute("age", 22);
 		return "display";
 	}
+	
+	@GetMapping("/send2")
+	public String sendData2(ModelMap map) {
+		map.addAttribute("id", 105);
+		map.addAttribute("name", "Surve");
+		map.addAttribute("age", 40);
+		return "display";
+	}
+	
+	@GetMapping("/send3")
+	public ModelAndView sendData3() {
+		
+		ModelAndView mav = new ModelAndView("display");
+		mav.addObject("id", 106);
+		mav.addObject("name", "ppp");
+		mav.addObject("age", 30);
+		return  mav;
+	}
+	
+	
 }
